83f1ddb7ac87c18843bb7861800c5a2b93bd4375
Vansh Agrawal <143166967+im-vansh@users.noreply.github.com>
2025-10-17 17:07:20 +0530

    Update testimonial.js

--- data/testimonial.js
+++ data/testimonial.js
@@ -5,7 +5,7 @@ export const testimonial = [
     author: "Sarah Chen",
     image: "https://randomuser.me/api/portraits/women/75.jpg",
     role: "Software Engineer",
-    company: "Tech Giant Co.",
+    company: "Google",
   },
   {
     quote:
@@ -13,14 +13,38 @@ export const testimonial = [
     author: "Michael Rodriguez",
     image: "https://randomuser.me/api/portraits/men/75.jpg",
     role: "Product Manager",
-    company: "StartUp Inc.",
+    company: "Meta",
   },
   {
     quote:
       "My resume's ATS score improved significantly. Got more interviews in two weeks than in six months!",
     author: "Priya Patel",
     image: "https://randomuser.me/api/portraits/women/74.jpg",
-    role: "Marketing Director",
-    company: "Global Corp",
+    role: "Vice President",
+    company: "Goldman Sachs",
+  },
+  {
+    quote:
+      "The interview prep feature helped me a lot in revising the concepts & tackling down my weak areas.",
+    author: "Soham Parekh",
+    image: "https://randomuser.me/api/portraits/men/77.jpg",
+    role: "Senior Software Engineer",
+    company: "Amazon",
+  },
+  {
+    quote:
+      "The Quizes is a perfect way to revise the older concepts and test our knowledge in the particular topics.",
+    author: "Angelina",
+    image: "https://randomuser.me/api/portraits/women/70.jpg",
+    role: "MTS-2",
+    company: "Adobe",
+  },
+  {
+    quote:
+      "The quick edit option makes it easy to update the resume and download it with one click.",
+    author: "Mark Hemilton",
+    image: "https://randomuser.me/api/portraits/men/7.jpg",
+    role: "Manager",
+    company: "Microsoft",
   },
 ];
